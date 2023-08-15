85b2c482c35f10503f351484a774fdca382dd233
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-08-15 10:09:05 -0700

    Updating 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,17 @@
  * Return: zero
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
+	int i;
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
