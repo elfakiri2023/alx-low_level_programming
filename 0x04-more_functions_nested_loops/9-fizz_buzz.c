4b9553a3cf4dce292f9f5efba17bcd0859d769b5
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-08-17 10:29:54 -0700

    Updating 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test.
+ * Description: You can only use _putchar function to print.
+ */
+
+void main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			_putchar("FizzBuzz");
+		}
+		else if ((n % 3) == 0)
+		{
+			_putchar("Fizz");
+		}
+		else if ((n % 5) == 0)
+		{
+			_putchar("Buzz");
+		}
+		else
+		{
+			_putchar("%d", n);
+		}
+
+		_putchar(" ");
+		n++;
+	}
+}
+
