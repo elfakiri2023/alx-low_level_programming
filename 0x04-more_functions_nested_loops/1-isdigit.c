6ede3b61efdd5519f4bde37abb6c89f4b4d2c657
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-08-17 06:33:44 -0700

    Updating 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - checks for a digit (0 through 9).
  * @c: init type number
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is digit, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
