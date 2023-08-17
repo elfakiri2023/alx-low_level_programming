ed76c0d93efe37b94147d5be796a5f0b08982619
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-08-17 07:21:29 -0700

    Updating 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - Print the numbers from 0 to 9
  * and skip 2 and 4.
- * Description: only use _putchar 2 times.
+ * Description: you can only use _putchar 2 times.
  */
 void print_most_numbers(void)
 {
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if (x != 2 || x != 4)
+		if (x != 2 && x != 4)
 		{
 			_putchar(x + '0');
 		}
