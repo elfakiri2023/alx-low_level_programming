34dbc0b39de6d0c6a32c50b796941c04ce5541ff
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-08-17 07:29:45 -0700

    Updating 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers,
+ * Description: from 0 to 14, followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int n, x;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar (x + '0');
+		}
+
+		_putchar ('\n');
+	}
+}
