76909d4627136c88c6bece2e93edb9285661efc0
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-01 06:34:15 -0700

    Updating 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file
+ * @argc: argument count
+ * @argv: arguments array
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
