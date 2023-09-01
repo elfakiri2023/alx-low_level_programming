cab7b0919ca15ee6649a30172bf9602a9be88716
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-01 07:22:25 -0700

    Updating 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the number of arguments
+ * @argc: number
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
