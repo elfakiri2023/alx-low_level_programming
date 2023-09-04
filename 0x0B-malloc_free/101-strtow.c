4068d5fada071e85488a1943929f8ca5a59e4f70
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-04 07:51:48 -0700

    Updating main.h

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * counterr - helper function to count the number of words in a string
+ * @s: string
+ * Return: number
+ */
+int counterr(char *s)
+{
+	int f, c, w;
+
+	f = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+		{
+			f = 0;
+		}
+		else if (f == 0)
+		{
+			f = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+
+/**
+ * **strtow - Splits a string into words.
+ * @str: string
+ * Return: pointer
+ */
+
+char **strtow(char *str)
+{
+	char **m, *t;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = counterr(str);
+	if (words == 0)
+	{
+		return (NULL);
+	}
+
+	m = (char **) malloc(sizeof(char *) * (words + 1));
+	if (m == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				t = (char *) malloc(sizeof(char) * (c + 1));
+				if (t == NULL)
+				{
+					return (NULL);
+				}
+				while (start < end)
+					*t++ = str[start++];
+				*t = '\0';
+				m[k] = t - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	m[k] = NULL;
+
+	return (m);
+}
