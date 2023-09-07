129f476db57b8d14f6a040b5823b17a6289896ad
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-07 06:38:00 -0700

    Updating main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,4 @@ int cal_strlen(char *string);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
