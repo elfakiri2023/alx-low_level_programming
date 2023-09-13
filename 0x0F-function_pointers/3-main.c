50421b79eb444854dd81c1ecdcc5d6aae7f68d7a
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-13 11:27:04 -0700

    Updating 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,45 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - function
- * @argc: int
- * @argv: char
+ * main - program that operations
+ * @argc: number
+ * @argv: array
  * Return: zero
  */
-
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*o)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = get_op_func(argv[2]);
-	if (o == NULL)
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	result = func(arg1, arg2);
 
-	printf("%d\n", o(a, b));
+	printf("%d\n", result);
 
 	return (0);
 }
