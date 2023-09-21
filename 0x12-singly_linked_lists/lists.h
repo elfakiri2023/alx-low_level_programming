f874d9573510728907f537705e0e58f7c9f6e490
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-21 04:39:56 -0700

    Updating 0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - a linked list.
+ * @str: char
+ * @len: the length
+ * @next: the next node.
+ * Description: list node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
