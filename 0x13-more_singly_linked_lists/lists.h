d81b2d436788bcf43ffa1c8f31ea7153fbf34068
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-25 12:19:15 -0700

    Updating lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - linked list
+ * @n: int
+ * @next: pointer
+ * Description: singly linked.
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct listp_s - singly linked
+ * @p: pointer
+ * @next: pointer
+ * Description: list of pointers
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 
 #endif
