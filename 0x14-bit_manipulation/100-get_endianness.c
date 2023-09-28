165fd21b902606e0e6ce51019ffa6b1aa4b9029f
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-28 09:02:54 -0700

    Updating 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - check the endianness.
+ * Return: 0 if big.
+ */
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
