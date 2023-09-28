8d4af75a9cf3064db2a61394ecde1ecc6a6d6563
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-09-28 08:20:28 -0700

    Updating 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number.
+ * @n: number
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = calculates_pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
+
+
+/**
+ * calculates_pow - calculates pow
+ * @base: base
+ * @power: power
+ * Return: value of base power
+ */
+unsigned long int calculates_pow(unsigned int base, unsigned int power)
+{
+	unsigned long int n;
+	unsigned int i;
+
+	n = 1;
+	for (i = 1; i <= power; i++)
+		n *= base;
+	return (n);
+}
