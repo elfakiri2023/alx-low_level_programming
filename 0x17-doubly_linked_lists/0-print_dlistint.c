86da77d8f34258f91a326ad8e74db45bf79d5b14
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-11-09 06:03:30 -0800

    Updating lists.h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - the elements of dlistint_t
+ * @h: head
+ * Return: number
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+
+	if (h == NULL)
+		return (c);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
