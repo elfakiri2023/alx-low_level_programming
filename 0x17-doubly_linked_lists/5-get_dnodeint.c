a9fb2c8414e264c52af458e19aff1227ade63471
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-11-09 06:12:36 -0800

    Updating 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth
+ * @head: head
+ * @index: index
+ * Return: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int j;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	j = 0;
+
+	while (head != NULL)
+	{
+		if (j == index)
+			break;
+		head = head->next;
+		j++;
+	}
+
+	return (head);
+}
